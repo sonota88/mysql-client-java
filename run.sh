9369fb7e73a27c22fead9e071f445ea1ed892214
sonota88 <yosiot8753@gmail.com>
2020-05-30 16:05:56 +0900

    Change jar path (Use print_project_dir)

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+print_project_dir() {
+  (
+    cd "$(dirname "$0")"
+    pwd
+  )
+}
+
+PROJECT_DIR="$(print_project_dir)"
+
 java -jar \
-  target/sample2-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
+  ${PROJECT_DIR}/target/sample2-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
   "$@"
